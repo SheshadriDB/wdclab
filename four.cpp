b101592a2892b1e8231d4c12139b1dc401f43bd9
XYZIOTLAB <84629530+SheshadriDB@users.noreply.github.com>
2023-11-02 12:40:04 +0530

    Update four.cpp

--- four.cpp
+++ four.cpp
@@ -1,49 +1,46 @@
-#include <stdio.h>
-#include<ctype.h>
+#include<stdio.h>
 #include<conio.h>
 #include<string.h>
-#define MAX 1000
-int main()
+#define N strlen(g)
+char t[128],cs[128],g[]="10110";
+int a,e,c;
+void xor(){
+for (c=1;c<N;c++)cs[c]=((cs[c]==g[c])?'0':'1');
+}
+void crc()
 {
-	{
-		int s,pi,ci;
-		char plain[MAX],cipher[MAX];
-		printf("***encryption , decr\n\n");
-			printf("\n enter:\n");
-			gets(plain);
-		while(1)
-		{
-			printf("\n Key(number of shifts per char)for encry:");
-				scanf("%d",&s);
-			if(s<1||s>25)
-				printf("bad input");
-			else
-				break;
-		}
-		printf("after removing");
-		for(ci=0,pi=0;plain[pi]!='\0';pi++)
-			if(isalpha(plain[pi]))
-				return 0;
-			{
-				putchar(toupper(plain[pi]));
-				cipher[ci++]=((toupper(plain[pi])-'a')+s%26)%26+'a';
-			}
-			cipher[ci]='\0';
-			printf("after encry");
-			while(1)
-			{
-				printf("key");
-				scanf("%D",&s);
-				if(s<1||s>25)
-					printf("bad");
-				else
-					break;
-			}
-			for(pi=0,ci=0;cipher[ci]!='\0';ci++)
-				plain[pi++]=((cipher[ci]-'a')+(26-s))%26+'a';
-			plain[pi]='\0';
-			printf("aft decrp");
-			//return 0;
+	for(e=0;e<N;e++)cs[e]=t[e];
+	do{
+		if(cs[0]=='1')xor();
+		for (c=0;c<N-1;c++)cs[c]=cs[c+1];
+		cs[c]=t[e++];
+	}
+	while(e<=a+N-1);
+}
+void main (){
+	printf("\n enterpoly:");scanf("%s",t);
+	printf("\n generating polynomial is:%s",g);
+	a=strlen(t);
+	for(e=a;e<a+N-1;e++)t[e]='0';
+	printf("\n modified t[u] is:%s",t);
+	crc();
+	printf("\n checksum is:%s",cs);
+	  
+	//rx
+	for(e=a;e<a+N-1;e++)t[e]=cs[e-a];
+	printf("\n final codeword is:%s",t);
+	int check,gg;
+	printf("\n test error detection 0(yes) 1(no)?");
+	scanf("%d",&check);
+	if(check==0){
+		printf("\n enter position for insert error:");
+		scanf("%d",&gg);
+		t[gg]=(t[gg]=='1'?'0':'1');
+		printf("\n errorneous data:%s:",t);
+			printf("\n error: ");
+	}
+	else {
+		printf("\n no error");
 	}
 	getch();
 }
